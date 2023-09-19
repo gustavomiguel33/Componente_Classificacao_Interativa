1d25cf6667eb9709f237d18eae03d332e6edb4dd
Gustavo <gustavosouzamiguel@outlook.com>
2023-09-19 20:59:58 -0300

    configurando o javascriot

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let botaoEnvia = document.querySelector('.botaoEnviar')
-let imagemEstrela = document.querySelector('.imagemEstrela')
+let estrela = document.querySelector('.estrela')
+let titulo1 = document.querySelector('.titulo1')
 
 botaoEnvia.addEventListener('click', function() {
-  
+  estrela.style.backgroundImage = "url('../img/illustration-thank-you.svg')"
+  estrela.style.width = "120px"
+
+  titulo1.classList.add('estiloTitulo1')
+  titulo1.innerText = 'You selected 4 out of 5'
 })
